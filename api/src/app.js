6a082d216285d2e5812dc174b1bd06aecf77addc
Eduardo Backes <e.backes2011@gmail.com>
2019-08-10 15:16:06 -0300

    exception handler

--- api/src/app.js
+++ api/src/app.js
@@ -3,6 +3,9 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import { resolve } from 'path';
+import Youch from 'youch';
+
+import 'express-async-errors';
 
 import routes from './routes';
 
@@ -14,6 +17,7 @@ class App {
 
     this.middlewares();
     this.routes();
+    this.expectionHandler();
   }
 
   middlewares() {
@@ -28,6 +32,18 @@ class App {
 
   routes() {
     this.server.use(routes);
+  }
+
+  expectionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+
+        return res.status(500).json(errors);
+      }
+
+      return res.status(500).json({ error: 'Internal server error' });
+    });
   }
 }
 
