96a564c3162d75f6c7045b7d3264d637b9475ae8
Eduardo Backes <e.backes2011@gmail.com>
2019-08-12 04:02:37 -0300

    user info at header

--- front/src/components/Header/index.js
+++ front/src/components/Header/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { logout } from '../../store/modules/auth/actions';
 import history from '../../services/history';
 
-import { Container, Content, StyledLink, Button } from './styles';
+import { Container, Content, StyledLink, Button, User } from './styles';
 
 import logo from '../../assets/logo_maior.png';
 
 export default function Header() {
   const dispatch = useDispatch();
+
+  const profile = useSelector(store => store.user.profile);
 
   function handleLogout() {
     dispatch(logout());
@@ -40,9 +42,24 @@ export default function Header() {
             Profile
           </StyledLink>
 
-          <Button type="button" onClick={handleLogout}>
-            Logout
-          </Button>
+          <User>
+            <img
+              src={
+                (profile.avatar && profile.avatar.url) ||
+                'https://api.adorable.io/avatars/285/abott@adorable.png'
+              }
+              alt=""
+            />
+
+            <ul>
+              <li>{profile.name}</li>
+              <li>
+                <Button type="button" onClick={handleLogout}>
+                  Logout
+                </Button>
+              </li>
+            </ul>
+          </User>
         </nav>
       </Content>
     </Container>
--- front/src/components/Header/styles.js
+++ front/src/components/Header/styles.js
@@ -43,4 +43,54 @@ export const StyledLink = styled(Link)`
 
 export const Button = styled(CustomButton)`
   padding: 0 20px;
+  width: 100%;
+`;
+
+export const User = styled.div`
+  position: relative;
+  cursor: pointer;
+
+  img {
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    border: 3px solid #a7a7d6;
+  }
+
+  ul {
+    position: absolute;
+    right: 0;
+    top: 60px;
+    visibility: hidden;
+    opacity: 0;
+    background: #fff;
+    padding: 20px;
+    /* border: 1px solid rgba(0, 0, 0, 0.75); */
+    box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.75);
+    z-index: 1;
+    transition: 0.3s all;
+
+    li {
+      white-space: nowrap;
+      color: #5c5c76;
+      font-size: 16px;
+      font-weight: bold;
+      padding-bottom: 10px;
+      margin-bottom: 10px;
+      border-bottom: 1px dotted #ccc;
+
+      &:last-child {
+        padding-bottom: 0;
+        margin-bottom: 0;
+        border: 0;
+      }
+    }
+  }
+
+  &:hover {
+    ul {
+      visibility: visible;
+      opacity: 1;
+    }
+  }
 `;
